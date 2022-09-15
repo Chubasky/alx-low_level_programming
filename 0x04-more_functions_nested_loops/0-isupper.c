3c70b167a4624bb3539051b8cfd81286fa9f9ad2
Chubasky <chukwunonso.nwagbo@gmail.com>
2022-09-15 12:46:41 -0700

    commit statement

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _isupper - functions that check for uppercase character
- * @c: the character
- * Return: 1 if c is uppecase overwise 0
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
+
 	}
-
-	return (0);
-
+	else
+	{
+		return (0);
+	}
 }
